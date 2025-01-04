2edb0759aeff93f5c00358c06b698c36167f806c
liu.bian.shi <liu.bian.shi@gmail.com>
2025-01-04 15:46:57 +0800

    refactor(opts): config history path

--- lua/icon-picker/init.lua
+++ lua/icon-picker/init.lua
@@ -1,108 +1,41 @@
 local ICON_TYPE = { "history", "alt_font", "emoji", "html_colors", "nerd_font", "symbols" }
-local HISTORY_FILE = vim.fn.stdpath("data") .. "/icon-picker-history.txt"
-
 local M = {}
 
 local function get_icons_history()
-	local hist = io.open(HISTORY_FILE, "r")
+	local hist = io.open(M.history_path, "r")
 	if not hist then
-		return {}
+		return { icons = {}, spaces = 0, sorted_keys = {} }
 	end
 
 	local data = {}
-  local keys = {}
+	local keys = {}
 
 	for line in hist:lines() do
 		local columns = vim.split(line, "%s+")
 
 		if #columns > 2 then
 			local key = table.concat(columns, " ", 2)
-			data[key] = {columns[1], columns[2]}
-      table.insert(keys, 1, key)
+			data[key] = { columns[1], columns[2] }
+			table.insert(keys, 1, key)
 		end
 	end
 	hist:close()
-  return { icons = data, spaces = 0, sorted_keys = keys}
+	return { icons = data, spaces = 0, sorted_keys = keys }
 end
-
-local icon_type_data = {
-	['history'] = get_icons_history(),
-  ["alt_font"] = {
-		icons = require("icon-picker.icons.alt-fonts"),
-		spaces = 6,
-	},
-	["emoji"] = {
-		icons = require("icon-picker.icons.emoji-list"),
-		spaces = 6,
-	},
-	["nerd_font"] = {
-		icons = require("icon-picker.icons.nf-icon-list"),
-		spaces = 7,
-	},
-	["nerd_font_v3"] = {
-		icons = require("icon-picker.icons.nf-v3-icon-list"),
-		spaces = 7,
-	},
-	["symbols"] = {
-		icons = require("icon-picker.icons.symbol-list"),
-		spaces = 7,
-	},
-	["html_colors"] = {
-		icons = require("icon-picker.icons.html-colors"),
-		spaces = 6,
-	},
-}
-
-local list_types = {
-	["PickAltFont"] = {
-		icon_types = { "alt_font" },
-		desc = "Pick an Alt Font Character",
-	},
-	["PickAltFontAndSymbols"] = {
-		icon_types = { "alt_font", "symbols" },
-		desc = "Pick Alt Font Characters & Symbols",
-	},
-	["PickEmoji"] = {
-		icon_types = { "emoji" },
-		desc = "Pick an emoji",
-	},
-	["PickIcons"] = {
-		icon_types = { "emoji", "nerd_font" },
-		desc = "Pick an Emoji & Nerd Font Icons",
-	},
-	["PickEverything"] = {
-		icon_types = ICON_TYPE,
-		desc = "Pick an icon",
-	},
-	["PickNerd"] = {
-		icon_types = { "nerd_font" },
-		desc = "Pick a Nerd Font character",
-	},
-	["PickNerdV3"] = {
-		icon_types = { "nerd_font_v3" },
-		desc = "Pick a Nerd Font V3 character",
-	},
-	["PickSymbols"] = {
-		icon_types = { "symbols" },
-		desc = "Pick a Symbol",
-	},
-	["PickHTMLColors"] = {
-		icon_types = { "html_colors" },
-		desc = "Pick an hexa HTML Color",
-	},
-}
 
 -- vim.ui.select functionality
 local function insert_user_choice_history(choice)
 	local columns = vim.split(choice, "%s+")
 	local key = table.concat(columns, " ", 3)
-	if icon_type_data.history.icons[key] then
+	if vim.tbl_get(M.icon_type_data, "history", "icons", key) then
 		return
 	end
 
-	icon_type_data.history.icons[key] = {columns[1], columns[2]}
-	local hist = io.open(HISTORY_FILE, "a")
-	if not hist then return end
+	M.icon_type_data.history.icons[key] = { columns[1], columns[2] }
+	local hist = io.open(M.history_path, "a")
+	if not hist then
+		return
+	end
 	hist:write(choice, "\n")
 	hist:close()
 end
@@ -157,19 +90,16 @@ end
 -- @param  map: hash map of pairs to insert into list
 -- @param  num_spaces: number of spaces to insert between key and val
 local function push_map(type_key, map, num_spaces, cur_list)
+	cur_list = cur_list or {}
 	if type_key == "history" then
-    if not icon_type_data.history.sorted_keys then return end
-    for _, key in ipairs(icon_type_data.history.sorted_keys) do
-      local val = map[key]
+		if not M.icon_type_data.history.sorted_keys then
+			return
+		end
+		for _, key in ipairs(M.icon_type_data.history.sorted_keys) do
+			local val = map[key]
 			key = string.format("%-16s", val[2]) .. key
 			val = val[1]
-			table.insert(
-				cur_list,
-				table.concat(
-					{ val, key },
-					string.rep(" ", 8 - vim.fn.strdisplaywidth(val))
-				)
-			)
+			table.insert(cur_list, table.concat({ val, key }, string.rep(" ", 8 - vim.fn.strdisplaywidth(val))))
 		end
 		return cur_list
 	end
@@ -180,10 +110,7 @@ local function push_map(type_key, map, num_spaces, cur_list)
 		for key, val in pairs(map) do
 			table.insert(
 				cur_list,
-				table.concat(
-					{ val, type_key .. key },
-					string.rep(" ", 8 - vim.fn.strdisplaywidth(val))
-				)
+				table.concat({ val, type_key .. key }, string.rep(" ", 8 - vim.fn.strdisplaywidth(val)))
 			)
 		end
 		return cur_list
@@ -196,23 +123,6 @@ local function push_map(type_key, map, num_spaces, cur_list)
 	return cur_list
 end
 
---- create list of icons and descriptions for UI and call UI with list
--- @param  keys: array table of icon list key names
--- @param  desc: description of picker functionality, e.g. "Pick an emoji"
--- @param  callback: user input callback function
-local function generate_list(keys, desc, callback)
-	local cur_tbl
-	local item_list = {}
-
-	for _, type_key in ipairs(keys) do
-		cur_tbl = icon_type_data[type_key]
-		item_list = push_map(type_key, cur_tbl["icons"], cur_tbl["spaces"], item_list)
-	end
-
-	custom_ui_select(item_list, desc, callback)
-end
-
--- New API
 function Split(s, delimiter) -- from code grep
 	local result = {}
 	for match in (s .. delimiter):gmatch("(.-)" .. delimiter) do
@@ -223,71 +133,81 @@ function Split(s, delimiter) -- from code grep
 	return result
 end
 
-local new_API_table = {
-	-- need better variable name
-	["Normal"] = insert_user_choice_normal,
-	["Insert"] = insert_user_choice_insert,
-	["Yank"] = yank_user_choice_normal,
-}
-
--- loops through the table & create user commands
-for command, callback in pairs(new_API_table) do
-	vim.api.nvim_create_user_command("IconPicker" .. command, function(opts)
-		local args = Split(opts.args, " ") -- split command arguments
-		local desc = "Pick"
-
-		if #args == 0 then
-			args = ICON_TYPE
-		end
-
-		local item_list = {}
-		for _, argument in ipairs(args) do
-			local cur_tbl = icon_type_data[argument]
-			if cur_tbl == nil then
-				return
-			end
-
-			-- push icon results into item_list
-			item_list = push_map(argument, cur_tbl["icons"], cur_tbl["spaces"], item_list)
-			desc = desc .. " " .. argument
-		end
-
-		custom_ui_select(item_list, desc, callback)
-	end, {
-		nargs = "?",
-		complete = function()
-			return ICON_TYPE
-		end,
-	})
+local function generate_icon_type_data()
+	return {
+		["history"] = get_icons_history(),
+		["alt_font"] = {
+			icons = require("icon-picker.icons.alt-fonts"),
+			spaces = 6,
+		},
+		["emoji"] = {
+			icons = require("icon-picker.icons.emoji-list"),
+			spaces = 6,
+		},
+		["nerd_font"] = {
+			icons = require("icon-picker.icons.nf-icon-list"),
+			spaces = 7,
+		},
+		["nerd_font_v3"] = {
+			icons = require("icon-picker.icons.nf-v3-icon-list"),
+			spaces = 7,
+		},
+		["symbols"] = {
+			icons = require("icon-picker.icons.symbol-list"),
+			spaces = 7,
+		},
+		["html_colors"] = {
+			icons = require("icon-picker.icons.html-colors"),
+			spaces = 6,
+		},
+	}
 end
 
--- init all commands (legacy)
-for type, data in pairs(list_types) do
-	vim.api.nvim_create_user_command(type, function()
-		generate_list(data["icon_types"], data["desc"], insert_user_choice_normal)
-	end, {})
+local function generate_api()
+	local API_table = {
+		-- need better variable name
+		["Normal"] = insert_user_choice_normal,
+		["Insert"] = insert_user_choice_insert,
+		["Yank"] = yank_user_choice_normal,
+	}
 
-	vim.api.nvim_create_user_command(type .. "Yank", function()
-		generate_list(data["icon_types"], data["desc"], yank_user_choice_normal)
-	end, {})
+	-- loops through the table & create user commands
+	for command, callback in pairs(API_table) do
+		vim.api.nvim_create_user_command("IconPicker" .. command, function(opts)
+			local args = Split(opts.args, " ") -- split command arguments
+			local desc = "Pick"
 
-	vim.api.nvim_create_user_command(type .. "Insert", function()
-		generate_list(data["icon_types"], data["desc"], insert_user_choice_insert)
-	end, {})
+			if #args == 0 then
+				args = ICON_TYPE
+			end
+
+			local item_list = {}
+			for _, argument in ipairs(args) do
+				local cur_tbl = M.icon_type_data[argument]
+				if cur_tbl == nil then
+					return
+				end
+
+				-- push icon results into item_list
+				item_list = push_map(argument, cur_tbl["icons"], cur_tbl["spaces"], item_list)
+				desc = desc .. " " .. argument
+			end
+
+			custom_ui_select(item_list, desc, callback)
+		end, {
+			nargs = "?",
+			complete = function()
+				return ICON_TYPE
+			end,
+		})
+	end
 end
 
 M.setup = function(opts)
-	if opts.disable_legacy_commands then
-		for type, _ in pairs(list_types) do
-			local status_ok, _ = pcall(vim.api.nvim_del_user_command, type)
-			if not status_ok then
-				return
-			end
-
-			vim.api.nvim_del_user_command(type .. "Insert")
-			vim.api.nvim_del_user_command(type .. "Yank")
-		end
-	end
+	M.history_path = opts.history_path or vim.fn.stdpath("data") .. "/icon-picker-history.txt"
+	M.icon_type_data = generate_icon_type_data()
+	vim.print(vim.inspect(M.icon_type_data.history))
+	generate_api()
 end
 
 return M
